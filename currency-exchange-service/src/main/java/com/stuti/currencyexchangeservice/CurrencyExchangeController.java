4d8675a37e1502910a977b6cddab6bf425379e72
megha <stutimegha2019@gmail.com>
2023-12-11 12:26:39 +0530

    add zipkin

--- currency-exchange-service/src/main/java/com/stuti/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/stuti/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,16 +1,17 @@
 package com.stuti.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -20,6 +21,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
        // CurrencyExchange currencyExchange = new CurrencyExchange(1L ,from,to, BigDecimal.valueOf(82));
+        logger.info("retrieveExchangeValue called with {} to {}",from,to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from,to);
         if(currencyExchange ==null) {
             throw new RuntimeException
